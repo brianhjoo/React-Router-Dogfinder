63343437be2711fc7e7d833d1a3fb1f7fcfc4cbc
Brian Joo <brianhjoo@gmail.com>
2023-04-24 15:32:03 -0700

    made DogDetails component

---
+++ src/DogDetails.js
@@ -0,0 +1,25 @@
+import { useParams } from 'react-router-dom';
+
+
+/** Details about a single dog
+ *
+ * Props:
+ *  - dog: data about a single dog
+ *
+ * State: none
+ *
+ * App --> DogList --> DogDetails
+ */
+function DogDetails() {
+  const { name } = useParams();
+
+  return (
+    <div className='DogDetails'>
+      <h2>{ name }</h2>
+      <img src={`./public/${name}`} alt={`dog ${name}`} />
+    </div>
+  );
+}
+
+
+export default DogDetails;
