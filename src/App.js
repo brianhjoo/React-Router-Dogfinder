6503de8f9a38fd1829362595868482277e51e345
Alyssa Denary <alyssa.denary@gmail.com>
2023-04-24 19:11:35 -0400

    add routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import DogList from './DogList.js';
+import DogDetails from './DogDetails';
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/dogs" element={ <DogList />} />
+          <Route path="/dogs/:name" element={ <DogDetails />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
