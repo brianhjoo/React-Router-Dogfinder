935e7b9604466e5c206b2786081c2e17982967c0
Alyssa Denary <alyssa.denary@gmail.com>
2023-04-24 19:10:32 -0400

    add file for api call helper fx

---
+++ src/api.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const BASE_URL = "localhost:5001";
+
+/** getAllDogs
+ * API call to get info on all dogs
+ *
+ * expected info:
+ *
+*/
+
+async function getAllDogs() {
+  const response = await axios.get(`${BASE_URL}/dogs`);
+
+  return response.data;
+}
+
+export { getAllDogs };
