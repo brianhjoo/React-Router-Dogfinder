055dd59b86362f3c753dc03a52d2303bfedafec1
Brian Joo <brianhjoo@gmail.com>
2023-04-24 16:59:31 -0700

    added getAllDogs to DogList comp.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import DogList from './DogList.js';
 import DogDetails from './DogDetails';
+import { getAllDogs } from './api';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/dogs" element={ <DogList />} />
+          <Route path="/dogs" element={ <DogList dogs={getAllDogs()} />} />
           <Route path="/dogs/:name" element={ <DogDetails />} />
         </Routes>
       </BrowserRouter>
