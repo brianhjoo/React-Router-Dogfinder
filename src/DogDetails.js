503438aa6fafdd33595b193b69a8d68fcffe0e9f
Alyssa Denary <alyssa.denary@gmail.com>
2023-04-24 21:19:56 -0400

    add dog details

--- src/DogDetails.js
+++ src/DogDetails.js
@@ -3,19 +3,28 @@ import { useParams } from 'react-router-dom';
 
 /** Details about a single dog
  *
- * Props: none
+ * Props:
+ * -dogs [{ name, age, facts }...]
  *
  * State: none
  *
  * App --> DogList --> DogDetails
  */
-function DogDetails() {
+function DogDetails({ dogs }) {
   const { name } = useParams();
+
+  const dog = dogs.find(d => d.name === name);
 
   return (
     <div className='DogDetails'>
       <h2>{ name }</h2>
-      <img src={`./public/${name}`} alt={`dog ${name}`} />
+      <img src={`./${name}.jpg`} alt={`dog ${name}`} />
+      <p>Age: {dog.age}</p>
+      <ul>
+        {dog.facts.map(f => {
+          <li>{f}</li>
+        })}
+      </ul>
     </div>
   );
 }
