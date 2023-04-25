91578151f5ea2781c3cd6376619eeddb5480b179
Alyssa Denary <alyssa.denary@gmail.com>
2023-04-24 20:32:54 -0400

    refactor with Dogs render

--- src/DogList.js
+++ src/DogList.js
@@ -3,35 +3,26 @@ import { useState } from 'react';
 import { getAllDogs } from './api';
 import { Link } from 'react-router-dom';
 
-/** List of all the dogs
+/** Display List of all the dogs
  *
  * Props:
- *  -
+ *  -dogs [{ name, age, facts }...]
  *
- * State:
- *  -dogsInfo
+ * State none
  *
- * App --> DogDetails
+ * Dogs --> DogList --> DogDetails
  */
+
 function DogList({ dogs }) {
-  const [dogsInfo, setDogsInfo] = useState(null);
-
-  async function getDogs(){
-    const dogs = await getAllDogs();
-
-    setDogsInfo(dogs);
-  }
 
   function showDogsLinks() {
-    return dogsInfo.map(({name, age, facts}, i) => {
+    return dogs.map(({ name, age, facts }, i) => {
       return (
         <div key={i}>
           <Link to={`./dogs/${name}`}>
             <img src={`./${name}.jpg`} alt={`dog ${name}`} />
             <h2>{name}</h2>
           </Link>
-          <p>age: {age}</p>
-          <p>facts: {facts}</p>
         </div>
       );
     });
@@ -39,12 +30,9 @@ function DogList({ dogs }) {
 
   return (
     <div className="DogList">
-      <button onClick={getDogs}>Get dogs</button>
-      {!dogsInfo && <p>Loading...</p>}
-      {dogsInfo && <div>{showDogsLinks()}</div>}
+      <div>{showDogsLinks()}</div>
     </div>
   );
 }
-
 
 export default DogList;
